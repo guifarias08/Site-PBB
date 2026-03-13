1e8e58bb588c97d5b122ae102d03a786ed5953e8
Guilherme Pinho <guilhermepinhofariasdeaquino@gmail.com>
2026-03-13 16:21:18 -0300

    Primeiro commit

---
+++ pbb.js
@@ -0,0 +1,23 @@
+
+const observerOptions = {
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.card').forEach((el) => observer.observe(el));
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
